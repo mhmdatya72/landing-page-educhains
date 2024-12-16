ba6d00b8a5d7103af1aa22b49ffabdb975783c6d
mhmdatya72 <alahly723@gmail.com>
2024-12-16 15:17:32 +0200

    Adding some modifications to the events

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -361,22 +361,26 @@
             <!-- Event Item -->
             <div class="row event_item">
                 <div class="col">
-                    <div class="row d-flex flex-row align-items-center justify-content-start">
+                    <div class="row d-flex flex-row align-items-end">
 
-                        <div class="col-lg-4">
-                            <div class="event_image_container">
-                                <img src="{{ asset('images/event_1.jpg') }}" alt="Event 1">
+                        <div class="col-lg-2 order-lg-1 order-2">
+                            <div class="event_date d-flex flex-column align-items-center justify-content-center">
+                                <div class="event_day">07</div>
+                                <div class="event_month">January</div>
                             </div>
                         </div>
 
-                        <div class="col-lg-8">
+                        <div class="col-lg-6 order-lg-2 order-3">
                             <div class="event_content">
-                                <div class="event_title">Workshop on Digital Marketing</div>
-                                <div class="event_text">
-                                    <p>Join our workshop to learn the latest trends in digital marketing and how to leverage social media platforms effectively. Limited seats available!</p>
-                                </div>
-                                <div class="event_date">Date: 15 December 2024</div>
-                                <div class="event_location">Location: Educhains Training Center</div>
+                                <div class="event_name"><a class="trans_200" href="#">Student Festival</a></div>
+                                <div class="event_location">Grand Central Park</div>
+                                <p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor.</p>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 order-lg-3 order-1">
+                            <div class="event_image">
+                                <img src="images/event_1.jpg" alt="https://unsplash.com/@theunsteady5">
                             </div>
                         </div>
 
@@ -387,22 +391,26 @@
             <!-- Event Item -->
             <div class="row event_item">
                 <div class="col">
-                    <div class="row d-flex flex-row align-items-center justify-content-start">
+                    <div class="row d-flex flex-row align-items-end">
 
-                        <div class="col-lg-4">
-                            <div class="event_image_container">
-                                <img src="{{ asset('images/event_2.jpg') }}" alt="Event 2">
+                        <div class="col-lg-2 order-lg-1 order-2">
+                            <div class="event_date d-flex flex-column align-items-center justify-content-center">
+                                <div class="event_day">07</div>
+                                <div class="event_month">January</div>
                             </div>
                         </div>
 
-                        <div class="col-lg-8">
+                        <div class="col-lg-6 order-lg-2 order-3">
                             <div class="event_content">
-                                <div class="event_title">Open Day for Career Development</div>
-                                <div class="event_text">
-                                    <p>Attend our Open Day to explore the various programs we offer and meet industry professionals who can help guide your career path.</p>
-                                </div>
-                                <div class="event_date">Date: 5 January 2025</div>
-                                <div class="event_location">Location: Educhains Training Center</div>
+                                <div class="event_name"><a class="trans_200" href="#">Open day in the Univesrsity campus</a></div>
+                                <div class="event_location">Grand Central Park</div>
+                                <p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor.</p>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 order-lg-3 order-1">
+                            <div class="event_image">
+                                <img src="images/event_2.jpg" alt="https://unsplash.com/@claybanks1989">
                             </div>
                         </div>
 
@@ -413,22 +421,26 @@
             <!-- Event Item -->
             <div class="row event_item">
                 <div class="col">
-                    <div class="row d-flex flex-row align-items-center justify-content-start">
+                    <div class="row d-flex flex-row align-items-end">
 
-                        <div class="col-lg-4">
-                            <div class="event_image_container">
-                                <img src="{{ asset('images/event_3.jpg') }}" alt="Event 3">
+                        <div class="col-lg-2 order-lg-1 order-2">
+                            <div class="event_date d-flex flex-column align-items-center justify-content-center">
+                                <div class="event_day">07</div>
+                                <div class="event_month">January</div>
                             </div>
                         </div>
 
-                        <div class="col-lg-8">
+                        <div class="col-lg-6 order-lg-2 order-3">
                             <div class="event_content">
-                                <div class="event_title">Job Placement Fair</div>
-                                <div class="event_text">
-                                    <p>Meet top recruiters and find exciting career opportunities in various sectors during our Job Placement Fair.</p>
-                                </div>
-                                <div class="event_date">Date: 20 February 2025</div>
-                                <div class="event_location">Location: Educhains Job Fair</div>
+                                <div class="event_name"><a class="trans_200" href="#">Student Graduation Ceremony</a></div>
+                                <div class="event_location">Grand Central Park</div>
+                                <p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor.</p>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 order-lg-3 order-1">
+                            <div class="event_image">
+                                <img src="images/event_3.jpg" alt="https://unsplash.com/@juanmramosjr">
                             </div>
                         </div>
 
@@ -437,6 +449,7 @@
             </div>
 
         </div>
+
     </div>
 </div>
 
